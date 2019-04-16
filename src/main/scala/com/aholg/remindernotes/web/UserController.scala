375116dc74eebd0e29579e46b7c0a2856b81d190
aholg <holgersson31@hotmail.com>
2019-04-16 21:51:47 +0200

    Add save and get user routes

--- src/main/scala/com/aholg/remindernotes/web/UserController.scala
+++ src/main/scala/com/aholg/remindernotes/web/UserController.scala
@@ -2,15 +2,20 @@ package com.aholg.remindernotes.web
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives
-import com.aholg.remindernotes.repository.{NoteRepository, UserRepository}
+import com.aholg.remindernotes.repository.{NoteRepository, User, UserRepository}
 import com.aholg.remindernotes.web.Common.{CommonProtocols, ErrorResponse}
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
 import scala.util.{Failure, Success}
 
 //TODO: Separate user functions to separate trait to ease testing
 class UserController(noteRepository: UserRepository) extends Directives with CommonProtocols {
 
-  val routes = (post & pathPrefix("users" / "save") & pathEndOrSingleSlash) {
+  import UserControllerProtocol._
+
+  val routes = concat(addUserRoute, getUserRoute)
+
+  private def addUserRoute = (post & pathPrefix("users" / "save") & pathEndOrSingleSlash) {
     formFields('userName, 'email, 'password) { (userName, email, password) =>
       onComplete(noteRepository.addUser(userName)) {
         case Success(_) => complete(StatusCodes.Created)
@@ -18,4 +23,15 @@ class UserController(noteRepository: UserRepository) extends Directives with Com
       }
     }
   }
+
+  private def getUserRoute = (get & pathPrefix("users" / Segment) & pathEndOrSingleSlash) { userId =>
+    onComplete(noteRepository.getUser(userId)) {
+      case Success(Some(user)) => complete(user)
+      case Failure(ex) => complete(StatusCodes.InternalServerError, ErrorResponse(ex.getMessage))
+    }
+  }
+}
+
+object UserControllerProtocol extends DefaultJsonProtocol {
+  implicit val userFormat: RootJsonFormat[User] = jsonFormat1(User)
 }
--- src/test/scala/com/aholg/remindernotes/web/UserControllerTest.scala
+++ src/test/scala/com/aholg/remindernotes/web/UserControllerTest.scala
@@ -1,8 +1,43 @@
 package com.aholg.remindernotes.web
 
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.marshalling.PredefinedToEntityMarshallers._
+import akka.http.scaladsl.model.Multipart.FormData
+import akka.http.scaladsl.model.Multipart.FormData.BodyPart.Strict
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import org.scalatest.FunSuite
+import com.aholg.remindernotes.repository.{Note, NoteRepository, User, UserRepository}
+import com.aholg.remindernotes.web.Common.{CommonProtocols, ErrorResponse}
+import com.aholg.remindernotes.web.NoteController.{NoteProtocols, NoteViewModel}
+import org.scalatest.{FunSuite, Matchers}
 
-class UserControllerTest extends FunSuite with ScalatestRouteTest{
+import scala.concurrent.Future
 
+class UserControllerTest extends FunSuite with ScalatestRouteTest with SprayJsonSupport with Matchers {
+
+  import UserControllerProtocol._
+
+  val controller = new UserController(new UserRepository {
+    override def addUser(username: String): Future[Unit] = Future.successful(Unit)
+
+    override def getUser(username: String): Future[Option[User]] = Future.successful(Some(User(username)))
+  })
+
+  test("should be able to save a user") {
+    Post("/users/save",
+      FormData(
+        Strict("userName", "remind me to fucking keeeeelll yoouuuuu"),
+        Strict("email", "blood"),
+        Strict("password", "blood"))) ~> Route.seal(controller.routes) ~> check {
+      status shouldBe StatusCodes.Created
+    }
+  }
+
+  test("should be able to retrieve a user") {
+    Get("/users/FartBlaster69") ~> Route.seal(controller.routes) ~> check {
+      status shouldBe StatusCodes.OK
+      responseAs[User] shouldEqual User("FartBlaster69")
+    }
+  }
 }
